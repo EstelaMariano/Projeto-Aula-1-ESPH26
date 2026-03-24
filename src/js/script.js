5f1cfddf5b9d250c3c35a81dc567c6139dce52e6
Estela <estela.mariano@icloud.com>
2026-03-24 11:44:20 -0300

    feat: Declarações e variáveis

---
+++ src/js/script.js
@@ -0,0 +1,51 @@
+// Declarações e Variáveis
+
+// var, let e const
+
+//var x=10;
+//console.log(x);
+//var x = 20;
+//console.log(x);
+
+// var pode redeclarar e acessar fora do escopo
+// ler não pode redeclarar e não consegue acessar fora do escopo
+
+//if(true){
+//    var nome="Fiap";
+//}
+//
+//console.log(nome)
+
+// cont
+//const taxaJuros = 0.85;
+//taxaJuros = 0.5;
+
+//console.log(taxaJuros)
+
+
+var ex1="Olá dev var";
+console.log(ex1);
+
+let ex2="Olá dev let";
+console.log(ex2);
+
+const ex3="Olá dev const";
+console.log(typeof ex3);
+
+let ex4=20;
+console.log(ex4);
+
+let ex5=true;
+console.log(typeof ex4);
+
+let ex6={nome: "Fiap"};
+console.log(typeof ex6);
+
+let ex7=["Java", "Python", "C"];
+console.log(ex7);
+
+let ex8; // Declaração/ Variável indefinida
+console.log(ex8);
+
+let ex9=null;
+console.log(typeof ex9);
