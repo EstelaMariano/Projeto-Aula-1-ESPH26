ba7f0707b7741574c3635d5acfbbb4ccd62b2baf
Estela <estela.mariano@icloud.com>
2026-03-31 11:08:39 -0300

    feat: Exercício Operadores

--- src/js/script.js
+++ src/js/script.js
@@ -53,27 +53,42 @@
 
 // se coloca rum valor em string dentro de uma soma, vai mostrar o valor em string o o valor em int no lado um do outro
 
-const valor1=20;
-const valor2=10;
+// const valor1=20;
+// const valor2=10;
 
-console.log(valor1 + valor2);
-console.log(valor1 / valor2);
-console.log(valor1 - valor2);
-console.log(valor1 * valor2);
+// console.log(valor1 + valor2);
+// console.log(valor1 / valor2);
+// console.log(valor1 - valor2);
+// console.log(valor1 * valor2);
 
 
-// OPERADOS LÓGICOS
-console.log(valor1 > valor2);
-console.log(valor1 > 10 && valor2 !=20); //Os dois tem que ser verdadeiro para ser True
-console.log(valor2 < 5 || valor1 > 3); //um deles é verdadeiro para ser True
+// // OPERADOS LÓGICOS
+// console.log(valor1 > valor2);
+// console.log(valor1 > 10 && valor2 !=20); //Os dois tem que ser verdadeiro para ser True
+// console.log(valor2 < 5 || valor1 > 3); //um deles é verdadeiro para ser True
 
-// COMPARAÇÃO
-console.log(valor1 < valor2);
-console.log(valor1 >= valor2);
-console.log(valor1 <= valor2);
-console.log(valor1 != valor2);
-console.log(valor1 == valor2);
-console.log(valor1 === valor2);
+// // COMPARAÇÃO
+// console.log(valor1 < valor2);
+// console.log(valor1 >= valor2);
+// console.log(valor1 <= valor2);
+// console.log(valor1 != valor2);
+// console.log(valor1 == valor2);
+// console.log(valor1 === valor2);
 
-//CONCATENAÇÃO - texto para ser exibido ao lado da variável
-console.log("O valor é", valor2);
+// //CONCATENAÇÃO - texto para ser exibido ao lado da variável
+// console.log("O valor é", valor2);
+
+// Exercício
+// 1-
+const p = 50;
+const v = 35;
+const precoFinal = p - v;
+console.log("O preço final com desconto é", precoFinal);
+
+//2-
+const usuarioLogado = 1;
+const token = 100;
+const autorizado = (usuarioLogado === 1) && (token >= 10);
+console.log("Trasação autorizada com sucesso!", autorizado)
+
+//3-
